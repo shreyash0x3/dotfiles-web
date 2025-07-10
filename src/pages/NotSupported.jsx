d492d439e90a9aaa5a3a8b3e9d3a090e259390a3
shreyash0x3 <shreyashkakad03@gmail.com>
2025-07-10 14:49:02 +0530

    notSupported page for mobile and tablets

---
+++ src/pages/NotSupported.jsx
@@ -0,0 +1,9 @@
+
+const NotSupported = () => (
+    <div className="h-screen w-screen flex items-center justify-center flex-col text-center inter-light">
+        <h2>This website is not available on mobile or tablet.</h2>
+        <p>Please use a laptop or desktop computer.</p>
+    </div>
+);
+
+export default NotSupported;
