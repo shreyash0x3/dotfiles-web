41c33613ed4503af5464dd558d6edca0d8e4b4ca
shreyash0x3 <shreyashkakad03@gmail.com>
2025-07-15 13:39:16 +0530

    animateHomeText

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,21 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import Rice from "../assets/hypr-rice.png";
 import HyprRice from "../assets/hypr-rice2.png";
-import Himage from "../assets/home-image.jpg";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import { useEffect, useState } from "react";
 
 
 function Home() {
+    const words = ["Pick It", "Clone It", "Use It", "Tweak It", "Own It"];
+    const [index, setIndex] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setIndex((prev) => (prev + 1) % words.length);
+        }, 4000); 
+        return () => clearInterval(interval);
+    }, []);
+
     return (
         <>
 
@@ -17,17 +25,30 @@ function Home() {
                 <main className="flex-1">
                     <div className="flex items-center mt-[7%] justify-between">
                         <div className="hero ml-[16%]">
-                            <p className="inter-extra-bold-italic text-[45px]">Stole It</p>
+
+                            <AnimatePresence mode="wait">
+                                <motion.p
+                                    key={index}
+                                    className="inter-extra-bold-italic text-[45px]"
+                                    initial={{ opacity: 0, y: 20 }}
+                                    animate={{ opacity: 1, y: 0 }}
+                                    exit={{ opacity: 0, y: -20 }}
+                                    transition={{ duration: 0.5 }}
+                                >
+                                    {words[index]}
+                                </motion.p>
+                            </AnimatePresence>
+
                             <p className="inter-bold text-20px leading-5 mt-[10px]">Access dotfiles easily. <br />
                                 Pick what you like. <br />
                                 Make it yours.</p>
                         </div>
                         <motion.div
-                            whileHover={{scale: 1.2}}
+                            whileHover={{ scale: 1.3 }}
                             transition={{ type: "spring", stiffness: 300, damping: 20 }}
                             className="mr-[16%] mix-blend-luminosity hover:mix-blend-normal hover:cursor-pointer"
                         >
-                            { /* Image by https://github.com/1amSimp1e/dots*/ }
+                            { /* Image by https://github.com/1amSimp1e/dots*/}
                             <img src={HyprRice} alt="riced-os-image" className="w-[600px] h-[350px] rounded-xl" />
                         </motion.div>
                     </div>
